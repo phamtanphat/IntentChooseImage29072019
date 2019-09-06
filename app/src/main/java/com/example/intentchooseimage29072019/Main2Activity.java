1634a8ad077cbd61c408ca0d5f9f609a0d04d54c
phatdroid94 <phatdroid94@gmail.com>
2019-09-06 19:29:51 +0700

    a

--- app/src/main/java/com/example/intentchooseimage29072019/Main2Activity.java
+++ app/src/main/java/com/example/intentchooseimage29072019/Main2Activity.java
@@ -3,9 +3,11 @@ package com.example.intentchooseimage29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,16 +23,31 @@ public class Main2Activity extends AppCompatActivity {
         tableLayout = findViewById(R.id.tableLayout);
         mangAlbum = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.array_Animal)));
 
-
 //        cot = 3 => Imageview
 //        dong = 6 => tableRow
+
         int dong = 6;
         int cot = 3;
         for (int i = 0 ; i < dong ; i++){
             TableRow tableRow = new TableRow(this);
             for (int y = 0 ; y < cot ; y++){
                 ImageView imageView = new ImageView(this);
-                imageView.setImageResource(R.drawable.bo);
+                final int index =  cot * i + y;
+                if (index >= mangAlbum.size()){
+                    break;
+                }else{
+                    int valueHinhChon = getResources().getIdentifier(mangAlbum.get(index),"drawable",getPackageName());
+                    TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(350,350);
+                    layoutParams.setMargins(10 ,0,0,0);
+                    imageView.setLayoutParams(layoutParams);
+                    imageView.setImageResource(valueHinhChon);
+                    imageView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    });
+                }
                 tableRow.addView(imageView);
             }
             tableLayout.addView(tableRow);
