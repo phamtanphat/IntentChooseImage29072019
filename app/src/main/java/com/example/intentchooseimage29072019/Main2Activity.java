db20c95684f6d49729974d62ede902210a256b13
phatdroid94 <phatdroid94@gmail.com>
2019-09-06 18:47:09 +0700

    a

--- app/src/main/java/com/example/intentchooseimage29072019/Main2Activity.java
+++ app/src/main/java/com/example/intentchooseimage29072019/Main2Activity.java
@@ -3,6 +3,9 @@ package com.example.intentchooseimage29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,11 +13,27 @@ import java.util.Arrays;
 public class Main2Activity extends AppCompatActivity {
 
     ArrayList<String> mangAlbum ;
+    TableLayout tableLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        tableLayout = findViewById(R.id.tableLayout);
         mangAlbum = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.array_Animal)));
 
+
+//        cot = 3 => Imageview
+//        dong = 6 => tableRow
+        int dong = 6;
+        int cot = 3;
+        for (int i = 0 ; i < dong ; i++){
+            TableRow tableRow = new TableRow(this);
+            for (int y = 0 ; y < cot ; y++){
+                ImageView imageView = new ImageView(this);
+                imageView.setImageResource(R.drawable.bo);
+                tableRow.addView(imageView);
+            }
+            tableLayout.addView(tableRow);
+        }
     }
 }
